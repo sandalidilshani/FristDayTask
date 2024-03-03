371336c8ff81f463c8454e5c1be95e0b73e3e2ed
sandalidilshani <rkpsandalidilshani@gmail.com>
2024-03-03 08:41:54 +0530

    netest

--- test.js
+++ test.js
@@ -1,38 +1,31 @@
-//Named Functions
-function func_named() {
-  console.log("This is named function");
+var a = 5;
+var b = "5";
+let c = "5";
+
+if (a == b) {
+  console.log("a and b are same");
+} else {
+  console.log("a and b are diffrent");
 }
-func_named();
 
-//Anonymous Functions
-let func_ann = function () {
-  console.log("this is Anonymous Functions");
-};
-func_ann();
-
-//Arrow Functions
-func_arr = () => {
-  console.log("this is arrow funcion");
-};
-func_arr();
-
-//Generators:can stop the
-//execution of a function from anywhere inside the function
-function* numberInt() {
-  let Number = 10;
-  while (Number > 0) {
-    yield Number--;
-  }
+if (a === b) {
+  console.log("a and b are same");
+} else {
+  console.log("a and b are diffrent");
 }
-let gen = numberInt();
-console.log(gen.next().value);
-console.log(gen.next().value);
 
-numberInt();
+if (a == c) {
+  console.log("a and c are same");
+} else {
+  console.log("a and c are diffrent");
+}
 
-//Invoked Function
-//creating a new scope to avoid polluting the global namespac
-(function () {
-  const message = "Hello, world!";
-  console.log(message);
-})();
+if (a === c) {
+  console.log("a and c are same");
+} else {
+  console.log("a and c are diffrent");
+}
+
+console.log(a + b);
+console.log(a + c);
+console.log(b + c);
